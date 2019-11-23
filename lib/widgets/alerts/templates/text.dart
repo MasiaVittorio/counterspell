98aedad20e048ead07bd9eec95f9e235da9968a3
MasiaVittorio <masiavivittorio@gmail.com>
2019-11-23 14:31:04 +0100

    better licenses

--- lib/widgets/alerts/templates/text.dart
+++ lib/widgets/alerts/templates/text.dart
@@ -8,19 +8,37 @@ class TextAlert extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Color background = Theme.of(context).scaffoldBackgroundColor;
     return Material(
-      color: Theme.of(context).scaffoldBackgroundColor,
-      child: SingleChildScrollView(
-        physics: Stage.of(context).panelScrollPhysics(),
-        child: Column(
-          children: <Widget>[
-            if(title != null) AlertTitle(title),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(text),
+      color: background,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Positioned.fill(
+            child: SingleChildScrollView(
+              physics: Stage.of(context).panelScrollPhysics(),
+              child: Column(
+                children: <Widget>[
+                  if(title != null) SizedBox(height: AlertTitle.height),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(text),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          if(title != null) Positioned(
+            top: 0.0,
+            right: 0.0,
+            left: 0.0,
+            height: AlertTitle.height,
+            child: Container(
+              color: background.withOpacity(0.7),
+              child: AlertTitle(title),
+            ),
+          ),
+        ],
       ),
     );
   }

73f93a3164654ae8fb0d784e5220a9174edb01b2
MasiaVittorio <masiavivittorio@gmail.com>
2019-12-08 10:06:27 +0100

    github page

--- lib/models/app_model/actions.dart
+++ lib/models/app_model/actions.dart
@@ -3,28 +3,25 @@ import 'package:counter_spell_new/core.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class CSActions{
-    
-  static void review() async {
-    final String url = Platform.isAndroid ? CSUris.playStore: CSUris.appStore;
+
+  static void _launchUrl(String url) async {
     if (await canLaunch(url)) {
       await launch(url);
     } else {
       print('Could not launch $url');
     }
+  } 
+  static void review() async {
+    _launchUrl(Platform.isAndroid ? CSUris.playStore: CSUris.appStore);
   }
   static void mailMe() async {
-    if (await canLaunch(CSUris.mailAction)) {
-      await launch(CSUris.mailAction);
-    } else {
-      print('Could not launch $CSUris.mailAction');
-    }
+    _launchUrl(CSUris.mailAction);
   }
   static void chatWithMe() async {
-    if (await canLaunch(CSUris.telegramGroup)) {
-      await launch(CSUris.telegramGroup);
-    } else {
-      print('Could not launch $CSUris.telegramGroup');
-    }
+    _launchUrl(CSUris.telegramGroup);
+  }
+  static void githubPage() async {
+    _launchUrl(CSUris.githubPage);
   }
 
 }
--- lib/widgets/stageboard/panel/extended_components/panel_info.dart
+++ lib/widgets/stageboard/panel/extended_components/panel_info.dart
@@ -17,9 +17,33 @@ class PanelInfo extends StatelessWidget {
           Section([
             const AlertTitle("About CounterSpell", centered: false,),
             ListTile(
+              title: const Text("The developer"),
+              leading: const Icon(Icons.person_outline),
+              onTap: () => stage.showAlert(const Developer(), size: Developer.height),
+            ),
+            ListTile(
               title: const Text("Licenses"),
               leading: const Icon(Icons.info_outline),
               onTap: () => stage.showAlert(AlertLicenses(), size: DamageInfo.height),
+            ),
+            ListTile(
+              title: const Text("Source code"),
+              leading: const Icon(McIcons.github_circle),
+              onTap: () => stage.showAlert(ConfirmAlert(
+                warningText: "You'll be redirected to your browser on CounterSpell's  github page",
+                twoLinesWarning: true,
+                action: CSActions.githubPage,
+                confirmIcon: Icons.exit_to_app,
+                cancelColor: CSColors.delete,
+              ), size: ConfirmAlert.twoLinesheight),
+            ),
+          ]),
+          Section([
+            const SectionTitle("Actions"),
+            ListTile(
+              title: const Text("Feedback"),
+              leading: const Icon(Icons.favorite_border),
+              onTap: () => stage.showAlert(const FeedbackAlert(), size: FeedbackAlert.height),
             ),
             ListTile(
               title: const Text("Rate CounterSpell"),
@@ -31,28 +55,24 @@ class PanelInfo extends StatelessWidget {
               leading: const Icon(McIcons.thumb_up_outline),
               onTap: () => stage.showAlert(const Support(), size: Support.height),
             ),
-          ]),
-          Section([
-            const SectionTitle("Contacts"),
             ListTile(
-              title: const Text("The developer"),
-              leading: const Icon(Icons.person_outline),
-              onTap: () => stage.showAlert(const Developer(), size: Developer.height),
-            ),
-            ListTile(
-              title: const Text("Feedback"),
-              leading: const Icon(Icons.favorite_border),
-              onTap: () => stage.showAlert(const FeedbackAlert(), size: FeedbackAlert.height),
-            ),
-            ListTile(
-              title: const Text("Contact me"),
-              leading: const Icon(Icons.mail_outline),
-              onTap: () => stage.showAlert(const ConfirmEmail(), size: ConfirmEmail.height),
-            ),
-            ListTile(
-              title: const Text("Chat with me"),
-              leading: const Icon(McIcons.telegram),
-              onTap: () => stage.showAlert(const ConfirmTelegram(), size: ConfirmTelegram.height),
+              title: const Text("Contacts"),
+              leading: const Icon(McIcons.message_text_outline),
+              onTap: () => stage.showAlert(AlternativesAlert(
+                label: "Contact me the way you want",
+                alternatives: <Alternative>[
+                  Alternative(
+                    title: "Telegram",
+                    icon: McIcons.telegram,
+                    action: () => stage.showAlert(const ConfirmTelegram(), size: ConfirmTelegram.height),
+                  ),
+                  Alternative(
+                    title: "E-mail",
+                    icon: Icons.mail_outline,
+                    action: () => stage.showAlert(const ConfirmEmail(), size: ConfirmEmail.height),
+                  ),
+                ],
+              ), size: AlternativesAlert.heightCalc(2)),
             ),
           ]),
 

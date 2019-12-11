4cfe72bb76c435c3929fb12ddb677e046a73bbf9
MasiaVittorio <masiavivittorio@gmail.com>
2019-12-11 13:56:44 +0100

    small fixes

--- lib/widgets/stageboard/panel/extended_components/settings_components/appearance.dart
+++ lib/widgets/stageboard/panel/extended_components/settings_components/appearance.dart
@@ -9,7 +9,7 @@ class SettingsAppearance extends StatelessWidget {
     final StageData<CSPage,SettingsPage> stage = Stage.of(context);
 
     return Section([
-      const SectionTitle("Appearance"),
+      const AlertTitle("Appearance", centered: false,),
       settings.alwaysOnDisplay.build((_, alwaysOn) => SwitchListTile(
         value: alwaysOn,
         onChanged: settings.alwaysOnDisplay.set,

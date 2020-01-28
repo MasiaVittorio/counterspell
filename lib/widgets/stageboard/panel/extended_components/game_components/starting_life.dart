95a171ffe0b8410952c038958d54a3c2f6e34bd2
MasiaVittorio <masiavivittorio@gmail.com>
2020-01-28 14:34:52 +0100

    new starting life

--- lib/widgets/stageboard/panel/extended_components/game_components/starting_life.dart
+++ lib/widgets/stageboard/panel/extended_components/game_components/starting_life.dart
@@ -5,13 +5,61 @@ class StartingLifeTile extends StatelessWidget {
 
   const StartingLifeTile();
 
-  //function to build every radio button for 20 / 30 / 40 life totals
+
+  static const Map<int,String> lifeNames ={
+    20: 'Regular',
+    30: 'Two Headed Giants',
+    40: 'Commander',
+  };
+  @override
+  Widget build(BuildContext context){
+
+    final theme = Theme.of(context);
+    final bloc = CSBloc.of(context);
+    final settings= bloc.settings;
+    final startingLife = settings.startingLife;
+    final stage = Stage.of<CSPage,SettingsPage>(context);
+
+    return BlocVar.build2(
+      startingLife,
+      stage.themeController.primaryColorsMap,
+      builder: (_, life, colorMap)
+        => Section(<Widget>[
+            SectionTitle("Starting Life: ${lifeNames[life] ?? life}", animated: true),
+ 
+            Row(children: <Widget>[
+              for(final n in [20,30,40])
+                _buildButton(n, life, bloc, colorMap[CSPage.life]),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8.0,0.0,8.0,0.0),
+                child: Material(
+                  color: theme.colorScheme.onSurface.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(50),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(50),
+                    onTap: () => stage.showAlert(
+                      CustomStartingLife(),
+                      size: InsertAlert.height,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(McIcons.pencil_outline),
+                    )
+                  ),
+                ),
+              ),
+            ],),
+          ],
+        ),
+    );
+  }
+  
   Widget _buildButton(int value, int current, CSBloc bloc, Color color){
     return Expanded(
       child: InkWell(
         onTap: () => bloc.settings.startingLife.set(value),
         child: Padding(
-          padding: EdgeInsets.only(right:16.0),
+          padding: EdgeInsets.only(right:16.0, top: 3.0, bottom: 3.0),
           child: Row(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -31,51 +79,6 @@ class StartingLifeTile extends StatelessWidget {
     );
   }
 
-  @override
-  Widget build(BuildContext context){
 
-    //names for the different default starting life totals
-    final Map<int,String> _lfnm ={
-      20: 'Regular',
-      30: 'Two Headed Giants',
-      40: 'Commander',
-    };
-
-    final bloc = CSBloc.of(context);
-    final settings= bloc.settings;
-    final startingLife = settings.startingLife;
-    final stage = Stage.of<CSPage,SettingsPage>(context);
-
-    //rebuild every time the default life changes
-    return BlocVar.build2(
-      startingLife,
-      stage.themeController.primaryColorsMap,
-      builder: (_, life, colorMap)
-        => Column(
-          children: <Widget>[
-
-            ListTile(
-              leading: const Icon(McIcons.flag_outline),
-              trailing: const Icon(McIcons.pencil_outline),
-              title: AnimatedText(text:'Starting Life: ' + ( _lfnm[life] ?? '$life')),
-              onTap: () => stage.showAlert(
-                CustomStartingLife(),
-                size: InsertAlert.height,
-              )
-            ),
-
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                _buildButton(20, life, bloc, colorMap[CSPage.life]),
-                _buildButton(30, life, bloc, colorMap[CSPage.life]),
-                _buildButton(40, life, bloc, colorMap[CSPage.life]),
-              ],
-            ),
-          ],
-        ),
-    );
-  }
-  
 }
 

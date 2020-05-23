95e6375d823a92042c6283677a195399417a1ac7
MasiaVittorio <masiavivittorio@gmail.com>
2020-05-23 17:25:01 +0200

    still body / group performance

--- lib/widgets/stageboard/body/body.dart
+++ lib/widgets/stageboard/body/body.dart
@@ -20,51 +20,31 @@ class CSBody extends StatelessWidget {
     final themer = bloc.themer;
     final StageData<CSPage,SettingsPage> stage = Stage.of(context);
 
+    //padding to account for the half collapsed panel that is visible over the body
+    final bottom = stage.dimensionsController.dimensions.value.collapsedPanelSize/2;
 
     return stage.themeController.derived.mainPageToPrimaryColor.build((_, pageColors){
       print("body building 2 (pageColors)");
-      return LayoutBuilder(builder: (_, constraints){
-        print("body building 3 (constraints)");
-        return group.names.build((_, names){
-          print("body building 4 (names)");
-          return StageBuild.offMainPagesData<CSPage>((_, enabledPages, __, currentPage){
-            print("body building 5 (main pages data)");
+      return themer.defenceColor.build((_, defenceColor){
+        print("body building 6 (defence color)");
+        return LayoutBuilder(builder: (_, constraints){
+          print("body building 3 (constraints)");
 
-            final bool landScape = constraints.maxWidth >= constraints.maxHeight;
-            final historyEnabled = enabledPages[CSPage.history];
-            if(landScape){
-              if(historyEnabled){
-                SchedulerBinding.instance.addPostFrameCallback((_) {
-                  //cant notify listeners during build phase lol
-                  print("disabling history");
-                  stage.mainPagesController.disablePage(CSPage.history);
-                });
-              }
-            } else {
-              if(!historyEnabled){
-                SchedulerBinding.instance.addPostFrameCallback((_) {
-                  //just (dont) build lol
-                  print("enabling history");
-                  stage.mainPagesController.enablePage(CSPage.history);
-                  bloc.game.gameHistory.listController.refresh(
-                    bloc.game.gameState.gameState.value.historyLenght,
-                  );
-                });
-              }
-            }
+          final bool landScape = constraints.maxWidth >= constraints.maxHeight;
+
+          final _sureConstraints = constraints.copyWith(
+            maxHeight: constraints.maxHeight - bottom,
+            minHeight: constraints.maxHeight - bottom,
+          );
+
+          return group.names.build((_, names){
+            print("body building 4 (names)");
 
             final int count = names.length;
             final int rowCount = landScape 
               ? (count / 2).ceil()
               : count;
 
-            //padding to account for the half collapsed panel that is visible over the body
-            final bottom = stage.dimensionsController.dimensions.value.collapsedPanelSize/2;
-
-            final _sureConstraints = constraints.copyWith(
-              maxHeight: constraints.maxHeight - bottom,
-              minHeight: constraints.maxHeight - bottom,
-            );
             final double tileSize = CSSizes.computeTileSize(
               _sureConstraints, 
               coreTileSize, 
@@ -73,30 +53,55 @@ class CSBody extends StatelessWidget {
 
             final double totalSize = tileSize * rowCount + bottom;
 
+            final Widget bodyHistory = BodyHistory(
+              bottom: bottom,
+              defenceColor: defenceColor,
+              pageColors: pageColors,
+              count: count,
+              tileSize: tileSize,
+              group: group,
+              names: names,
+              coreTileSize: coreTileSize,
+            );
+
             return ConstrainedBox(
               constraints: constraints,
               child: SingleChildScrollView(
                 child: SizedBox(
                   width: constraints.maxWidth,
                   height: totalSize,
-                  child: themer.defenceColor.build((_, defenceColor){
-                    print("body building 6 (defence color)");
+                  child: StageBuild.offMainPagesData<CSPage>((_, enabledPages, __, currentPage){
+                    print("body building 5 (main pages data)");
+
+                    final historyEnabled = enabledPages[CSPage.history];
+                    if(landScape){
+                      if(historyEnabled){
+                        SchedulerBinding.instance.addPostFrameCallback((_) {
+                          //cant notify listeners during build phase lol
+                          print("disabling history");
+                          stage.mainPagesController.disablePage(CSPage.history);
+                        });
+                      }
+                    } else {
+                      if(!historyEnabled){
+                        SchedulerBinding.instance.addPostFrameCallback((_) {
+                          //just (dont) build lol
+                          print("enabling history");
+                          stage.mainPagesController.enablePage(CSPage.history);
+                          bloc.game.gameHistory.listController.refresh(
+                            bloc.game.gameState.gameState.value.historyLenght,
+                          );
+                        });
+                      }
+                    }
+
                     return Stack(
                       fit: StackFit.expand,
                       children: <Widget>[
                         if(!landScape)
                           Positioned.fill(
                             right: CSSizes.minTileSize,
-                            child: BodyHistory(
-                              bottom: bottom,
-                              defenceColor: defenceColor,
-                              pageColors: pageColors,
-                              count: count,
-                              tileSize: tileSize,
-                              group: group,
-                              names: names,
-                              coreTileSize: coreTileSize,
-                            ),
+                            child: bodyHistory,
                           ),
 
                         AnimatedPositioned(
